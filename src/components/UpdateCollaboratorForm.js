33a562254fef78a0de00cbbd0cc34f4c9c070f6e
Abdelhamid CHEBEL <127632240+Abdelhamid2c@users.noreply.github.com>
2026-04-05 22:16:35 +0100

    Add 'IE Responsible' option to function selection

--- src/components/UpdateCollaboratorForm.js
+++ src/components/UpdateCollaboratorForm.js
@@ -42,6 +42,7 @@ const UpdateCollaboratorForm = ({ currentUser, onBack, onSuccess }) => {
   const functionOptions = [
     { value: '', label: 'Select a function' },
     { value: 'PE Responsible', label: 'PE Responsible' },
+    { value: 'IE Responsible', label: 'IE Responsible' },
     { value: 'PE Supervisor', label: 'PE Supervisor' },
     { value: 'IE Supervisor', label: 'IE Supervisor' },
     { value: 'PE Technician', label: 'PE Technician' },
